e80b806ddea778e9aaad18ed34bc511a76a91261
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2023-06-18 10:28:01 -0300

    change db

--- src/logic/firebase/config/app.ts
+++ src/logic/firebase/config/app.ts
@@ -3,7 +3,10 @@ import { initializeApp, FirebaseApp } from 'firebase/app'
 const app: FirebaseApp = initializeApp({
     apiKey: "AIzaSyBNJ-dhfrFMrlqgtuEBcvQbIytH0szUpwI",
     authDomain: "desc-bitcent.firebaseapp.com",
-    projectId: "desc-bitcent"
+    projectId: "desc-bitcent",
+    storageBucket: "desc-bitcent.appspot.com",
+    messagingSenderId: "591389067331",
+    appId: "1:591389067331:web:12d63feaae6c70980eecf6"
 })
 
 export { app }
